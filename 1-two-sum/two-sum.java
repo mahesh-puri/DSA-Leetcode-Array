798b6283ce2163dfc55f82dcb817a2d132120283
mahesh puri <77053102+mahesh-puri@users.noreply.github.com>
2025-03-10 18:45:05 +0530

    Time: 2 ms (98.83%) | Memory: 45.1 MB (29.53%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map<Integer, Integer> twoSumCompliments = new HashMap<>();
+
+        for(int index = 0 ; index < nums.length ; index++ ){
+            Integer complimentNum = target - nums[index];
+            
+            if(twoSumCompliments.containsKey(nums[index])){
+                Integer complimentNumIndex = twoSumCompliments.get(nums[index]);
+                return new int[]{complimentNumIndex, index};
+            }
+            twoSumCompliments.put(complimentNum, index);
+        }
+        return nums;
+    }
+}
