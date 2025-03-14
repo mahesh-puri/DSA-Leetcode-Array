7491237b6854ba13634410cf8fffe866ae9bc241
mahesh puri <77053102+mahesh-puri@users.noreply.github.com>
2025-03-15 04:39:54 +0530

    Time: 4 ms (90.47%) | Memory: 58.1 MB (42.89%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,18 +1,28 @@
 class Solution {
     public int maxArea(int[] height) {
-        
+        // Initialize variables:
+        // maxArea to keep track of the maximum water area found.
+        // left pointer starting from the beginning of the array.
+        // right pointer starting from the end of the array.    
         int maxArea = 0, left = 0, right = height.length - 1;
 
+        // Iterate while the left pointer is less than the right pointer.
         while(left < right){
-            
-            // int area = Math.min(height[left], height[right]) * (right - left);
+            // Calculate the area formed between the lines at the left and right pointers.
+            // The height is determined by the shorter line (to ensure water doesn't spill over),
+            // and the width is the distance between the two pointers.
             int area = (height[left] < height[right] ? height[left] : height[right]) * (right - left);
+            // int area = Math.min(height[left], height[right]) * (right - left);
 
-
+            // Update maxArea if the newly calculated area is greater.
             if(area > maxArea){
                 maxArea = area;
             }
 
+            // Move the pointers to explore potentially larger areas:
+            // - If the line at the left pointer is shorter, move the left pointer to the right.
+            // - Otherwise, move the right pointer to the left.
+            // This strategy aims to find taller lines that might form a larger area.
             if(height[left] < height[right]){
                 left++;
             } else {
